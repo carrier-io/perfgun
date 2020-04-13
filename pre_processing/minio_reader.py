6306938e47c4a50cbddf03abdd7d8e70a94db377
Mykhailo Hunko <mykhailo_hunko@epam.com>
2020-04-13 15:23:41 +0300

    Added project_id and OAToken

--- pre_processing/minio_reader.py
+++ pre_processing/minio_reader.py
@@ -1,6 +1,5 @@
 from os import environ
 import requests
-from urllib.parse import urlparse
 import zipfile
 from traceback import format_exc
 
@@ -9,16 +8,22 @@ BUCKET = environ.get("bucket")
 TEST = environ.get("artifact")
 PATH_TO_FILE = f'/tmp/{TEST}'
 TESTS_PATH = environ.get("tests_path", '/opt/gatling')
+PROJECT_ID = environ.get('project_id')
+TOKEN = environ.get("token")
 
 if (not all(a for a in [URL, BUCKET, TEST])):
     exit(0)
 
 try:
-    r = requests.get(f'{URL}/artifacts/{BUCKET}/{TEST}', allow_redirects=True)
+    if PROJECT_ID:
+        endpoint = f'/api/v1/artifacts/{PROJECT_ID}/{BUCKET}/{TEST}'
+    else:
+        endpoint = f'/artifacts/{BUCKET}/{TEST}'
+    headers = {'Authorization': f'bearer {TOKEN}'} if TOKEN else {}
+    r = requests.get(f'{URL}/{endpoint}', allow_redirects=True, headers=headers)
     with open(PATH_TO_FILE, 'wb') as file_data:
         file_data.write(r.content)
     with zipfile.ZipFile(PATH_TO_FILE, 'r') as zip_ref:
         zip_ref.extractall(TESTS_PATH)
 except Exception:
     print(format_exc())
-
