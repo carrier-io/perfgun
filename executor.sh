49c9988129605ed9bb346c7b0f21d0992eb6912d
hunkom <46744804+hunkom@users.noreply.github.com>
2020-07-15 20:35:26 +0300

    Use minio reader even when compile=true (#26)
    * Changed InfluxDB reporting. Updated post-processing
    
    * Changed InfluxDB reporting. Updated post-processing
    
    * Changed InfluxDB reporting. Updated post-processing
    
    * Added minio reader/poster. Added sampler_type tag
    
    * update README
    
    * Added project_id and OAToken
    
    * Added hits
    
    * Use minio reader when compile=true
    
    Co-authored-by: mykhailo_hunko <mykhailo.hunko@epam.com>

--- executor.sh
+++ executor.sh
@@ -118,8 +118,8 @@ fi
 
 export tests_path=/opt/gatling
 mkdir '/tmp/data_for_post_processing'
-if [[ "${compile}" != true ]]; then
 python /opt/gatling/bin/minio_reader.py
+if [[ "${compile}" != true ]]; then
 python /opt/gatling/bin/minio_args_poster.py -t $test_type -s $simulation_name -b ${build_id} -l ${lg_id} ${_influx_host} -p ${influx_port} -idb ${gatling_db} -en ${env} ${_influx_user} ${_influx_password}
 fi
 
